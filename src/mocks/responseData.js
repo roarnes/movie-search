88e9755fd23bac2b1b58e9ee0919d2895b388a7b
Arnes <respatiarnes@gmail.com>
2024-03-13 20:00:24 +0100

    Add mock response data

---
+++ src/mocks/responseData.js
@@ -0,0 +1,148 @@
+export const movieAutoCompleteResponse = [
+  'El padrino: The Latin Godfather',
+  'Our Godfather',
+  'The Godfather: Part III',
+  'Tokyo Godfathers',
+  'The Godfather Buck',
+  '3 Godfathers',
+  'The Godfather: Part II',
+  'The Godfather Trilogy: 1901-1980',
+  'The Godfather Legacy',
+  'The Black Godfather',
+  'The Godfather',
+]
+
+export const movieSearchResponse = [
+  {
+    id: 'tt10095336',
+    title: 'Our Godfather',
+    overview:
+      'The story of how Sicilian Mafia boss Tommaso Buscetta (1928-2000), the Godfather of Two Worlds, revealed, starting in 1984, the deepest secrets of the organization, thus helping to convict the hundreds of mafiosi who were tried in the trial held in Palermo between 1986 and 1987.',
+    poster:
+      'https://assets.movies.skruv.io/d/S/I/9/dSI9Gy5xqmObmKoR9D80Bl0c1GC.jpg',
+    runtime: 93,
+    popularity: 5.873,
+    genres: ['Documentary', 'History'],
+    released: '2019-04-28',
+  },
+  {
+    id: 'tt0068646',
+    title: 'The Godfather',
+    overview:
+      'Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.',
+    poster:
+      'https://assets.movies.skruv.io/3/b/h/k/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
+    runtime: 175,
+    popularity: 64.559,
+    genres: ['Drama', 'Crime'],
+    released: '1972-03-14',
+  },
+  {
+    id: 'tt0040064',
+    title: '3 Godfathers',
+    overview:
+      'Three outlaws on the run discover a dying woman and her baby. They swear to bring the infant to safety across the desert, even at the risk of their own lives.',
+    poster:
+      'https://assets.movies.skruv.io/d/s/V/r/dsVrYIpK2Rcgsr5QrfjBQWee5JE.jpg',
+    runtime: 106,
+    popularity: 8.276,
+    genres: ['Western'],
+    released: '1948-12-31',
+  },
+  {
+    id: 'tt0388473',
+    title: 'Tokyo Godfathers',
+    overview:
+      "During a Christmas Eve in Tokyo, three homeless people, middle-aged alcoholic Gin, former drag queen Hana, and dependent runaway girl Miyuki, discover an abandoned newborn while looking through the garbage. With only a handful of clues to the baby's identity, the three misfits search the city to find its parents.",
+    poster:
+      'https://assets.movies.skruv.io/u/k/h/v/ukhvxpVcBsb1MpRRwiqEIwyKdUX.jpg',
+    runtime: 93,
+    popularity: 10.293,
+    genres: ['Animation', 'Drama', 'Comedy'],
+    released: '2003-12-29',
+  },
+  {
+    id: 'tt10289996',
+    title: 'The Black Godfather',
+    overview:
+      'Follows the life of Clarence Avant, the ultimate, uncensored mentor and behind-the-scenes rainmaker in music, film, TV and politics.',
+    poster:
+      'https://assets.movies.skruv.io/3/B/Q/A/3BQArnXPwM5nHeS1tlGKLY7eapP.jpg',
+    runtime: 118,
+    popularity: 7.376,
+    genres: ['Documentary', 'Music'],
+    released: '2019-06-07',
+  },
+  {
+    id: 'tt13593766',
+    title: 'The Godfather Buck',
+    overview:
+      'Two brothers, who meet once a year at a cabin in Big Bear to hunt white-tailed deer for a week, uncover hidden family secrets and lies when their stepbrother drops by for an evening, changing their lives forever.',
+    poster:
+      'https://assets.movies.skruv.io/i/X/E/x/iXExHdQvEKi53Xwv55iMrc0CGE4.jpg',
+    runtime: 0,
+    popularity: 5.543,
+    genres: ['Drama'],
+    released: '2022-04-19',
+  },
+  {
+    id: 'tt2311160',
+    title: 'The Godfather Legacy',
+    overview:
+      "THE GODFATHER LEGACY goes deep inside Francis Ford Coppola's epic saga about the Corleone crime family and reveals how the Academy Award-winning film and its sequels became one of the most acclaimed franchises in Hollywood history. Featuring iconic scenes from all three films, never before seen home movies and insightful interviews with filmmakers (Francis Ford Coppola, All Ruddy and Peter Bart), actors (Al Pacino, James Caan, Talia Shire, Joe Mantegna, et al.), law enforcement officials and even former Mafia members. This feature length documentary illustrates why The Godfather trilogy continues to entertain and fascinate audiences and how it continues to impact the way society views everything from capitalism to crime.",
+    poster:
+      'https://assets.movies.skruv.io/9/y/9/w/9y9w5prvVjBtwWl9TNtvThL0NNi.jpg',
+    runtime: 95,
+    popularity: 5.554,
+    genres: ['Documentary'],
+    released: '2012-07-12',
+  },
+  {
+    id: 'tt0099674',
+    title: 'The Godfather: Part III',
+    overview:
+      'In the midst of trying to legitimize his business dealings in 1979 New York and Italy, aging mafia don, Michael Corleone seeks forgiveness for his sins while taking a young protege under his wing.',
+    poster:
+      'https://assets.movies.skruv.io/l/m/3/p/lm3pQ2QoQ16pextRsmnUbG2onES.jpg',
+    runtime: 170,
+    popularity: 36.634,
+    genres: ['Crime', 'Drama', 'Thriller'],
+    released: '1990-12-25',
+  },
+  {
+    id: 'tt0071562',
+    title: 'The Godfather: Part II',
+    overview:
+      'In the continuing saga of the Corleone crime family, a young Vito Corleone grows up in Sicily and in 1910s New York. In the 1950s, Michael Corleone attempts to expand the family business into Las Vegas, Hollywood and Cuba.',
+    poster:
+      'https://assets.movies.skruv.io/h/e/k/3/hek3koDUyRQk7FIhPXsa6mT2Zc3.jpg',
+    runtime: 202,
+    popularity: 44.34,
+    genres: ['Drama', 'Crime'],
+    released: '1974-12-20',
+  },
+  {
+    id: 'tt0150742',
+    title: 'The Godfather Trilogy: 1901-1980',
+    overview:
+      'The multigenerational saga of the rise and fall of the Corleone crime family. Story is told in chronological order, and numerous scenes that were deleted from each film have been restored.',
+    poster:
+      'https://assets.movies.skruv.io/r/w/E/P/rwEP0RwpF2mKGRbs7jerdC4aeMB.jpg',
+    runtime: 583,
+    popularity: 14.536,
+    genres: ['Crime', 'Drama'],
+    released: '1992-10-17',
+  },
+  {
+    id: 'tt0325950',
+    title: 'El padrino: The Latin Godfather',
+    overview:
+      'In the streets of East Los Angeles, Manny is a formidable drug dealer. Impressed by his extravagant lifestyle and prowess, his young son, Kilo, yearns to follow in his footsteps. Kilo resolves to learn how to prosper in the drug world, and his new life as a dealer begins. In a world where a man wants everything, he may end up with nothing.',
+    poster:
+      'https://assets.movies.skruv.io/1/0/J/7/10J7EQ8WvMYku8lcZrLewV2Ko4I.jpg',
+    runtime: 128,
+    popularity: 12.687,
+    genres: ['Action', 'Crime', 'Drama'],
+    released: '2004-09-27',
+  },
+]
