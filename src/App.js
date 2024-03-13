019c14b8f96d7a10967bdbf0e71a767836b5d880
Arnes <respatiarnes@gmail.com>
2024-03-13 03:53:29 +0100

    Show movie search results

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.scss'
 import SearchBar from './components/SearchBar'
+import SearchResults from './components/SearchResults'
+import useMovieSearch from './hooks/useMovieSearch'
 
 const App = () => {
+  const {
+    handleFetchMovieSearch,
+    movieSearchResults,
+    resetMovieSearchResults,
+  } = useMovieSearch()
+
+  const handleSearchMovie = (keyword) => {
+    if (!keyword) {
+      resetMovieSearchResults()
+      return
+    }
+    handleFetchMovieSearch({ keyword })
+  }
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar onSearch={handleSearchMovie} />
+      {movieSearchResults && <SearchResults results={movieSearchResults} />}
     </div>
   )
 }
